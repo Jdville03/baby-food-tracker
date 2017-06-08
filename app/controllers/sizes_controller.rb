a87401889b4eb45064ff93ec067985a32d91f9a0
Jdville03 <jarrel.delottinville@gmail.com>
2017-06-08 01:37:37 -0400

    update sizes controller and views

--- app/controllers/sizes_controller.rb
+++ app/controllers/sizes_controller.rb
@@ -1,7 +1,7 @@
 class SizesController < ApplicationController
 
   get '/sizes/new' do
-    
+
   end
 
   get '/sizes/:id/edit' do
@@ -15,7 +15,19 @@ class SizesController < ApplicationController
 
   patch '/sizes/:id' do
     redirect_if_not_logged_in
-
+    @size = Size.find_by_id(params[:id])
+    if @size && current_user.babies.include?(@size.baby)
+      @size.entry_date = params[:size][:entry_date]
+      @size.height = params[:size][:height]
+      @size.weight = params[:size][:weight]
+      if @size.save
+        redirect "/babies/#{@size.baby.slug}"
+      else
+        erb :'sizes/edit'
+      end
+    else
+      redirect "/users/#{current_user.slug}?error=You may only add or edit entries for your own babies."
+    end
   end
 
 
--- app/models/size.rb
+++ app/models/size.rb
@@ -2,4 +2,7 @@ class Size < ActiveRecord::Base
   belongs_to :baby
 
   validates :entry_date, presence: true, if: "weight.present? || height.present?"
+  validates :height, presence: { message: "must be given if weight is not entered" }, if: "entry_date.present? && !weight.present?"
+  validates :weight, presence: { message: "must be given if height is not entered" }, if: "entry_date.present? && !height.present?"
+
 end
