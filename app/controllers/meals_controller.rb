163cfe45c42124b86bf9daaae2fec7fc4c4a7ced
Jdville03 <jarrel.delottinville@gmail.com>
2017-06-09 03:45:25 -0400

    update meals controller and views

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -4,6 +4,17 @@ class MealsController < ApplicationController
 
   end
 
+  get '/meals/:id' do
+    redirect_if_not_logged_in
+    @meal = Meal.find_by_id(params[:id])
+    # user may only view meal of baby that belongs to user
+    if @meal && current_user.babies.include?(@meal.baby)
+      erb :'meals/show'
+    else
+      redirect "/users/#{current_user.slug}?error=You may only view entries for your own babies."
+    end
+  end
+
   get '/meals/:slug/new' do
 
   end
@@ -13,7 +24,14 @@ class MealsController < ApplicationController
   end
 
   get '/meals/:id/edit' do
-
+    redirect_if_not_logged_in
+    @meal = Meal.find_by_id(params[:id])
+    # user may only view meal of baby that belongs to user
+    if @meal && current_user.babies.include?(@meal.baby)
+      erb :'meals/edit'
+    else
+      redirect "/users/#{current_user.slug}?error=You may only view entries for your own babies."
+    end
   end
 
   patch '/meals/:id/edit' do
