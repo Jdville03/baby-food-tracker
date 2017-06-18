f1c633d71d8d5a92edb5d34aa5d67fda6656c569
Jdville03 <jarrel.delottinville@gmail.com>
2017-06-17 20:04:05 -0400

    update size validations

--- app/models/size.rb
+++ app/models/size.rb
@@ -3,7 +3,21 @@ class Size < ActiveRecord::Base
 
   # size object must have entry_date and one or both of height and weight
   validates :entry_date, presence: true
-  validates :height, presence: { message: "must be entered if weight is not entered" }, if: "entry_date.present? && !weight.present?"
-  validates :weight, presence: { message: "must be entered if height is not entered" }, if: "entry_date.present? && !height.present?"
+  validate :entry_date_invalid
+  validate :height_and_or_weight_must_be_present
+  validates :height, numericality: { greater_than_or_equal_to: 0.1 }, if: "height.present?"
+  validates :weight, numericality: { greater_than_or_equal_to: 0.1 }, if: "weight.present?"
+
+  def entry_date_invalid
+    if entry_date.present? && ((self.baby && entry_date < self.baby.birthdate) || entry_date > Date.current)
+      errors.add(:entry_date, "can't be before baby's birthdate or in the future")
+    end
+  end
+
+  def height_and_or_weight_must_be_present
+    if entry_date.present? && !height.present? && !weight.present?
+      errors.add(:height, "and/or weight must be entered")
+    end
+  end
 
 end
