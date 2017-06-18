f34651991f29ca22311dd09bd00cca91e7c3ccec
Jdville03 <jarrel.delottinville@gmail.com>
2017-06-17 20:18:28 -0400

    update meal and size validations

--- app/models/size.rb
+++ app/models/size.rb
@@ -1,13 +1,12 @@
 class Size < ActiveRecord::Base
   belongs_to :baby
 
-  # size object must have entry_date and one or both of height and weight
   validates :entry_date, presence: true
-  validate :entry_date_invalid
-  validate :height_and_or_weight_must_be_present
+  validate :entry_date_invalid, :height_and_or_weight_must_be_present
   validates :height, numericality: { greater_than_or_equal_to: 0.1 }, if: "height.present?"
   validates :weight, numericality: { greater_than_or_equal_to: 0.1 }, if: "weight.present?"
 
+  # size object must have valid entry_date and one or both of height and weight
   def entry_date_invalid
     if entry_date.present? && ((self.baby && entry_date < self.baby.birthdate) || entry_date > Date.current)
       errors.add(:entry_date, "can't be before baby's birthdate or in the future")
